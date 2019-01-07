5e3b740dcdcb34b1821168f1e861e4a46ebce581
BomCoding <masterbmc1@gmail.com>
2019-01-07 18:50:42 +0900

    스프링부트로 CRUD구현하기 리스트, 글쓰기, 수정, 삭제 가능 파일업로드 하는 중.

---
+++ workspace_bootTest/demo/src/main/java/com/example/demo/board/domain/FileVO.java
@@ -0,0 +1,40 @@
+package com.example.demo.board.domain;
+
+public class FileVO {
+	private int fno;
+	private int bno;
+	private String fileName; //저장할 파일
+	private String fileOnName; //실제파일
+	private String fileUrl;
+	
+	public int getFno() {
+		return fno;
+	}
+	public void setFno(int fno) {
+		this.fno = fno;
+	}
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getFileOnName() {
+		return fileOnName;
+	}
+	public void setFileOnName(String fileOnName) {
+		this.fileOnName = fileOnName;
+	}
+	public String getFileUrl() {
+		return fileUrl;
+	}
+	public void setFileUrl(String fileUrl) {
+		this.fileUrl = fileUrl;
+	}
+}
---
+++ workspace_bootTest/test/src/main/java/lee/main/controller/MainController.java
@@ -0,0 +1,5 @@
+package lee.main.controller;
+
+public class MainController {
+
+}
